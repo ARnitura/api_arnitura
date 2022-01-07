7bd9c6f8741688712046880d5caa6433741e9f54
vadimmazurek <asikpro13@mail.ru>
2022-01-08 01:31:47 +0500

    edit settings to reg ru

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, send_file
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def main():
     return send_file('files/model.glb')
 
 
 if __name__ == '__main__':
-    app.run(port=7001)
+    application.run(port=7001)
